a027e161f263b211938eb06acf47f970e670a66d
angelc99 <34936091+angelc99@users.noreply.github.com>
2019-10-18 10:20:36 +0700

    Create phone.java

---
+++ phone.java
@@ -0,0 +1,28 @@
+package tes;
+
+public class phone {
+	private String phoneName;
+	private int price;
+	
+	public void setPhoneName(String phoneName)
+	{
+		this.phoneName=phoneName;
+	}
+	
+	public void setPrice(int price)
+	{
+		this.price=price;
+	}
+	
+	public String getPhoneName()
+	{
+		return phoneName;
+	}
+	
+
+	public int setPrice()
+	{
+		return price;
+	}
+	
+}
